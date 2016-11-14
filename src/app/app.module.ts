fb9945c2512608d3dee59e76b443c84de9c245db
Robert <robert@tooconnect.nl>
2016-11-14 15:38:40 +0100

    Titel direct aanpasen
    ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
+import {routing} from "./app.router";
 
 import {AppComponent} from './app.component';
 import {NieuwsItemsService} from "./services/nieuws-items.service";
 import {NieuwsItemsComponent} from './components/nieuws-items/nieuws-items.component';
-import {routing} from "./app.router";
 import {NieuwsItemComponent} from './components/nieuws-item/nieuws-item.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 
@@ -20,6 +20,7 @@ import {NavbarComponent} from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpModule,
     routing,
--- src/app/components/nieuws-item/nieuws-item.component.ts
+++ src/app/components/nieuws-item/nieuws-item.component.ts
@@ -16,14 +16,13 @@ export class NieuwsItemComponent implements OnInit {
   nieuwsItemId:string;
   nieuwsItem: NieuwsItem;
 
-  myForm: FormGroup;
+  public myForm: FormGroup;
+  public submitted: boolean;
+  public events: any[]=[];
 
-  constructor(fb: FormBuilder, private router: Router, private route: ActivatedRoute,private nieuwsItemService: NieuwsItemsService) {
+  constructor(private fb: FormBuilder, private router: Router, private route: ActivatedRoute,private nieuwsItemService: NieuwsItemsService) {
 
-    this.myForm = fb.group({
-      titel: new FormControl(),
-      samenvattingOverzicht:new FormControl()
-    });
+
 
     setTimeout(()=> {
       this.paramsSub = route.params.subscribe(
@@ -33,7 +32,18 @@ export class NieuwsItemComponent implements OnInit {
         .subscribe(
           data => {
             this.nieuwsItem = data;
+            (<FormControl>this.myForm.controls['titel'])
+              .setValue(this.nieuwsItem.titel, { onlySelf: true });
 
+            (<FormControl>this.myForm.controls['samenvattingOverzicht'])
+              .setValue(this.nieuwsItem.samenvattingOverzicht, { onlySelf: true });
+
+            this.myForm.valueChanges
+             .subscribe(
+               nieuwsItem => this.nieuwsItem = nieuwsItem
+              );
+            //(<FormControl>this.myForm).setValue(this.nieuwsItem,{onlySelf: true});
+            //Dit werkt alleen als het hele model in de form is beschreven
 
             console.log(data)
           },
@@ -42,7 +52,24 @@ export class NieuwsItemComponent implements OnInit {
     },0);
   }
 
+
+
   ngOnInit() {
+    console.log("ngOnInit")
+    this.myForm = new FormGroup({
+      id: new FormControl(''),
+      titel: new FormControl('titel', [<any>Validators.required, <any>Validators.minLength(5)]),
+      samenvattingOverzicht: new FormControl(('samenvattingOverzicht'))
+
+    });
+
   }
 
+  save(nieuwsItem: NieuwsItem, isValid: boolean) {
+    this.submitted = true; // set form submit to true
+
+    // check if model is valid
+    // if valid, call API to save customer
+    console.log(nieuwsItem, isValid);
+  }
 }
